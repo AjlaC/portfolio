92368554f69d0b3fa63d81eeea4b32835e05a5fc
Ajla Cano <cano.ajla@gmail.com>
2019-11-06 20:34:57 +0100

    fixed client-side plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/light-timeline'],
+  plugins: [{src: '~/plugins/light-timeline', ssr:false}],
   /*
    ** Nuxt.js dev-modules
    */
