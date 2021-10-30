0e16754ff878e4298dac2a75984979fd67e79b8f
Filip Lindahl <filip.g.lindahl@gmail.com>
2021-10-30 21:51:09 +0200

    update role

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,14 +6,13 @@
     type="light"
     variant="light"
   >
-    <b-navbar-brand to="/" style="color: rgba(255,255,255,0.87)"
+    <b-navbar-brand to="/" style="color: rgba(255, 255, 255, 0.87)"
       ><span class="special-font">Ajla<b>Cano</b></span></b-navbar-brand
     >
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
     <b-collapse id="nav_collapse" is-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-item to="/" exact class="button">Home</b-nav-item>
-        <b-nav-item to="/about" class="button">About me</b-nav-item>
         <b-nav-item to="/portfolio" class="button">Portfolio</b-nav-item>
         <b-nav-item to="/contact" class="button">Contact</b-nav-item>
       </b-navbar-nav>
